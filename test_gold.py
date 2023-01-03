aed3750a7954e0f6caa6f19e94a620b74c4f6602
sagar H M <55372566+sagarhm@users.noreply.github.com>
2023-01-03 16:25:50 +0530

    Update test_gold.py

--- test_gold.py
+++ test_gold.py
@@ -1,24 +1,23 @@
 import unittest
 from unittest.mock import patch
-from MyRequest.gold import Gold
-import json
+from gold import Gold
 
 
 class Test_gold(unittest.TestCase):
 
     def test_gold(self):
 
-        fake_json = [{"Soumil":'Ok'}]
 
-        with patch('MyRequest.gold.requests.get') as mock_get:
+
+        with patch('gold.requests.get') as mock_get:
             mock_get.return_value.status_code = 200
-            mock_get.return_value.json.return_value = fake_json
+   
 
             obj = Gold()
             response = obj.get
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), fake_json)
+
 
 if __name__ == "__main__":
     unittest.main()
